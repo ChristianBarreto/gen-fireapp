f88b6b7dba50f6d5465ae392f1490f115dc89000
ChristianBarreto <christiannabasbarreto@gmail.com>
2026-03-29 08:08:49 -0300

    add active class on drawer menu

--- src/admin_app/AdminTemplate/TemplateDrawer/DrawerItem.tsx
+++ src/admin_app/AdminTemplate/TemplateDrawer/DrawerItem.tsx
@@ -2,7 +2,7 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 export default function AdminDrawerItem({
@@ -10,13 +10,15 @@ export default function AdminDrawerItem({
   url,
 }: {
   name: string
-  url: string
+  url: string 
 }) {
   const navigate = useNavigate();
-  
+  const location = useLocation();
+  const isActive = location.pathname === url
+
   return (
     <ListItem disablePadding>
-      <ListItemButton onClick={() => navigate(url)}>
+      <ListItemButton selected={isActive} onClick={() => navigate(url)}>
         <ListItemIcon>
           <ArrowForwardIosIcon />
         </ListItemIcon>
